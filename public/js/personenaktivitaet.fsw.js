c6dd17a9bfa916673791a0d898916020576bf777
Guenter Hipler <guenter.hipler@unibas.ch>
2014-11-15 11:56:56 +0100

    bei vorigem commit noch vergessen

---
+++ public/js/personenaktivitaet.fsw.js
@@ -0,0 +1,90 @@
+/**
+ * Created by swissbib on 13.11.14.
+ */
+
+
+var  Personenaktivitaet = {
+
+    init: function () {
+        //alert ('Personen.init()');
+        this.initSidebar();
+        this.initEditor();
+    },
+
+    initSidebar: function () {
+        //alert (this.constructor);
+        //alert('Personen.initSidebar');
+        Sidebar.init($.proxy(this.onSearchListUpdated, this), $.proxy(this.onContentUpdated, this));
+    },
+    initEditor: function () {
+        //alert ('Personen.initEditor()');
+        Editor.init($.proxy(this.onContentUpdated, this));
+
+        $('.updateCoverLink').click(function(event){
+
+            event.preventDefault();
+
+            var currentIndex = $(event.target).attr("data-currentIndex");
+
+            //name=zoradocs[0][oai_identifier]
+            var oaiIdentifier = $('input[name="zoradocs\\[' +  currentIndex + '\\][oai_identifier\\]"]',$('div#zoradocs')).val();
+
+
+            $("<div id='fswDialogBox'>").dialog({
+                open: function(){
+                    $(this).load('/personenaktivitaet/editCoverLink',
+                        {
+                            oai_identifier: oaiIdentifier,
+                            modus: 'show'
+                        }
+                    );
+
+                    $('#fswDialogBox').css('background-color','#d9d9d9');
+                },
+                buttons: [
+                    {
+                        text: "Sichern",
+                        click: function() {
+
+                            $(this).load('/personenaktivitaet/editCoverLink/' + oaiIdentifier );
+
+                        }
+                    },
+                    {
+                        text: "Abbrechen",
+                        click: function() {
+                            $( this ).dialog( "close" );
+                        }
+                    }
+
+                ],
+
+
+                close: function (event, ui) {
+                    $(this).dialog('destroy').remove();
+                },
+                title: 'Attribute einer Veranstaltung',
+                width: '1000px',
+                modal: true
+
+            }).dialog( "widget")
+                .find( ".ui-dialog-titlebar-close" )
+                .hide();
+
+        });
+
+
+    },
+    onContentUpdated: function () {
+        //alert ('Personen.onContentUpdated');
+        //FSWAdmin.PersonenUpdated = true;
+        this.initEditor();
+    },
+
+    onSearchListUpdated: function () {
+
+    }
+
+
+
+}
